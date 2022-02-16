c3222c56b001afbf64642579716adf6e447f9d3c
silvaan <s.siilvan@yahoo.com>
2022-02-16 22:53:40 +0600

    Get Input Value by Function

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-console.log('i am conntected')
+
 // Get input Value by function
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputText = inputField.value;
+    const inputAmount = parseFloat(inputText);
+    return inputAmount;
+}
+
+// Get Calculate button and add Event Listener
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    // get input Amount
+    const incomeAmount = getInputValue('income');
+    console.log(incomeAmount);
+
+})
