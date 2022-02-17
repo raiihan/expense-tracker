926fc29e8a626d26fd48d24c7fb4b847390d850d
silvaan <s.siilvan@yahoo.com>
2022-02-17 09:25:24 +0600

    Calculate Section with javascript

--- js/app.js
+++ js/app.js
@@ -12,5 +12,19 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     // get input Amount
     const incomeAmount = getInputValue('income');
     console.log(incomeAmount);
+    // get Expenses Amount
+    const foodAmount = getInputValue('food');
+    const rentAmount = getInputValue('rent');
+    const clothesAmount = getInputValue('clothes');
+    console.log('Food : ', foodAmount, "rent : ", rentAmount, 'clothes : ', clothesAmount)
+    // total Expense
+    const totalExpenses = foodAmount + rentAmount + clothesAmount;
+    console.log(totalExpenses);
+    // calculate Balance
+    const balance = incomeAmount - totalExpenses;
+    console.log(balance);
+    // update UI
+    document.getElementById('total-expenses').innerText = totalExpenses;
+    document.getElementById('balance').innerText = balance;
 
 })
